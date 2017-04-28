72d69e1e2e3271372ac48bef1c84c2ca3bf7b351
root <root@test.localdomain>
2017-04-28 13:41:33 +0000

    website

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="True-health">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/images/truehealth-1-234x128.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+  <link rel="stylesheet" href="assets/bootstrap-material-design-font/css/material.css">
+  <link rel="stylesheet" href="assets/et-line-font-plugin/style.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/animate.css/animate.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+<section id="menu-0">
+
+    <nav class="navbar navbar-dropdown navbar-fixed-top">
+        <div class="container">
+
+            <div class="mbr-table">
+                <div class="mbr-table-cell">
+
+                    <div class="navbar-brand">
+                        <a href="https://mobirise.com" class="navbar-logo"><img src="assets/images/truehealth-1-234x128.png" alt="Mobirise"></a>
+                        
+                    </div>
+
+                </div>
+                <div class="mbr-table-cell">
+
+                    <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="hamburger-icon"></div>
+                    </button>
+
+                    <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar"><li class="nav-item"><a class="nav-link link" href="overview.html">OVERVIEW</a></li><li class="nav-item dropdown open"><a class="nav-link link dropdown-toggle" href="" aria-expanded="true" data-toggle="dropdown-submenu">FEATURES</a><div class="dropdown-menu"><a class="dropdown-item" href="login.html">login</a><a class="dropdown-item" href="Products.html">Products</a><a class="dropdown-item" href="BMI.html">BMI</a></div></li></ul>
+                    <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="close-icon"></div>
+                    </button>
+
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+</section>
+
+<section class="engine"><a rel="external" href=""></a></section><section class="mbr-section mbr-section-hero mbr-section-full mbr-parallax-background mbr-section-with-arrow mbr-after-navbar" id="header1-1" style="background-image: url(assets/images/mbr-2000x1333.jpg);">
+
+    
+
+    <div class="mbr-table-cell">
+
+        <div class="container">
+            <div class="row">
+                <div class="mbr-section col-md-10 col-md-offset-1 text-xs-center">
+
+                    <h1 class="mbr-section-title display-1">WELCOME</h1>
+                    <p class="mbr-section-lead lead"></p>
+                    <div class="mbr-section-btn"><a class="btn btn-lg btn-primary" href="login.html">Login</a> <a class="btn btn-lg btn-white btn-white-outline" href="signup.html">Sign Up</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="mbr-arrow mbr-arrow-floating" aria-hidden="true"><a href="#footer1-2"><i class="mbr-arrow-icon"></i></a></div>
+
+</section>
+    
+    
+    </div>
+<section class="mbr-section article mbr-section__container" id="content7-7" style="background-color: rgb(255, 255, 255); padding-top: 20px; padding-bottom: 20px;">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-4 lead"><p>Kelsey Hibberd, from Southend, remembers her years at secondary school as being miserable. She intentionally kept her Facebook friends to a minimum because she knew they were the ones who wouldn't pick on her.</p><p>It was all about my body and how I looked</p><p>Kelsey Hibberd</p><p>"I'd always been tall, and I was a bit podgy too," she says.</p><p>"No-one seemed to notice at primary school, but then in Year 7 everyone started pointing at me, noticing things, making me think I was ugly and not special."</p><p>She became increasingly conscious of even tiny things such as the shape of her eyebrows and size of her forehead.</p><p>"I would have been subject to much more abuse if I'd had more friends on social media," she says.</p></div>
+            <div class="col-xs-12 col-md-4 lead"><p>She changed her hair colour and stopped eating to try to fit in before she realised that "it was for other people to stop hating on me".</p><p>Now, at 20, Kelsey is running a mentoring programme called Loud Education, which goes into schools to talk to pupils and train teachers on how to deal with body confidence issues. It also provides advice on any lesbian, gay, bisexual, and transgender issues pupils might have.</p><p>As a result, she is well aware of the power of social media. Teenagers can have thousands of "friends" online and that can leave them exposed, she says.</p><p>"You put forward your best self, and that can be a bit dangerous, because you naturally compare yourself to others," she says.</p><p>Among teenagers, Facebook, Twitter, Instagram and a number of other messaging apps, such as WhatsApp, are their main means of communicating with each other and the world.</p><p>In 2013, two-thirds of teenagers had signed up to the Facebook app, where images are posted and shared millions of times a day.</p></div>
+            <div class="col-xs-12 col-md-4 lead"><p>In 2012, MPs recommended that all schoolchildren should take part in compulsory body image and self-esteem lessons.</p><p>An inquiry by the All-Party Parliamentary Group on Body Image heard evidence that girls as young as five were worrying about their size and their appearance.</p><p>Adults were not immune from these negative attitudes either, with about 60% of the public feeling ashamed of the way they looked.</p><p>The MPs' report said pressure to look good had pushed up cosmetic surgery rates by nearly 20% since 2008.</p><p>MP Caroline Nokes was a member of the parliamentary group which, along with a number of charities, businesses and public bodies, is launching a campaign to change attitudes to body image, called Be Real: Body Confidence for Everyone</p></div>
+        </div>
+    </div>
+
+</section>
+
+<section class="mbr-section" id="msg-box6-m" data-bg-video="https://www.youtube.com/embed/F0pwXRQxSYE?rel=0&amp;amp;showinfo=0&amp;autoplay=0&amp;loop=0" style="padding-top: 120px; padding-bottom: 120px;">
+
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(0, 0, 0);">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="mbr-table-md-up">
+
+              <div class="mbr-table-cell mbr-right-padding-md-up mbr-valign-top col-md-7 image-size" style="width: 50%;">
+                  <div class="mbr-figure"><iframe class="mbr-embedded-video" src="https://www.youtube.com/embed/F0pwXRQxSYE?rel=0&amp;amp;showinfo=0&amp;autoplay=1&amp;loop=0" width="1280" height="720" frameborder="0" allowfullscreen></iframe></div>
+              </div>
+
+              
+
+
+              <div class="mbr-table-cell col-md-5 text-xs-center text-md-left content-size">
+                  <h3 class="mbr-section-title display-2">Body Image &amp; The Media</h3>
+                  <div class="lead">
+
+                    <p></p>
+
+                  </div>
+
+                  
+              </div>
+
+
+              
+
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="mbr-section mbr-section__container" id="map2-9" style="background-color: rgb(255, 255, 255); padding-top: 80px; padding-bottom: 80px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="mbr-map"><iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0Dx_boXQiwvdz8sJHoYeZNVTdoWONYkU&amp;q=place_id:ChIJc3FBGy2UcEgRmHnurvD-gco" allowfullscreen=""></iframe></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section mbr-section-md-padding" id="social-buttons3-a" style="background-color: rgb(46, 46, 46); padding-top: 90px; padding-bottom: 90px;">
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-xs-center">
+                <h3 class="mbr-section-title display-2">SHARE THIS PAGE!</h3>
+                <div>
+
+                  <div class="mbr-social-likes" data-counters="false">
+                    <span class="btn btn-social facebook" title="Share link on Facebook">
+                        <i class="socicon socicon-facebook"></i>
+                    </span>
+                    <span class="btn btn-social twitter" title="Share link on Twitter">
+                        <i class="socicon socicon-twitter"></i>
+                    </span>
+                    <span class="btn btn-social plusone" title="Share link on Google+">
+                        <i class="socicon socicon-googleplus"></i>
+                    </span>
+                    
+                    
+                  </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-footer mbr-section mbr-section-md-padding" id="contacts5-b" style="background-color: rgb(239, 239, 239); padding-top: 90px; padding-bottom: 90px;">
+
+
+    <div class="row">
+    
+            <div class="mbr-company col-xs-12 col-md-6 col-lg-3">
+
+                <div class="mbr-company card">
+                   
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <span class="list-group-icon"><span class="etl-icon icon-phone mbr-iconfont-company-contacts5"></span></span>
+                            <span class="list-group-text">+44874278234 or +1 (0) 000 0000 002</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="list-group-icon"><span class="etl-icon icon-map-pin mbr-iconfont-company-contacts5"></span></span>
+                            <span class="list-group-text">Birmingham, West Midlands</span>
+                        </li>
+                        <li class="list-group-item active">
+                            <span class="list-group-icon"><span class="etl-icon icon-envelope mbr-iconfont-company-contacts5"></span></span>
+                            <span class="list-group-text"><a href="mailto:support@true-health.uk">support@true-health.uk</a></span>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="mbr-footer-content col-xs-12 col-md-6 col-lg-3">
+                <h4>Categories</h4>
+                <ul><li><a class="text-primary" href="#">Business</a></li><li><a class="text-primary" href="#">Design</a></li><li><a class="text-primary" href="#">Real life</a></li><li><a class="text-primary" href="#">Science</a></li><li><a class="text-primary" href="#">Tech</a></li></ul>
+            </div>
+            <div class="mbr-footer-content col-xs-12 col-md-6 col-lg-3">
+                <p><strong>Contacts</strong><br>Email: info@true-health.uk<br>Phone: +447508203057<br><br><br><br><br><br></p>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3" data-form-type="formoid">
+
+                <div data-form-alert="true">
+                    <div hidden="" data-form-alert-success="true">Thanks for filling out form!</div>
+                </div>
+
+                <form action="https://mobirise.com/" method="post" data-form-title="MESSAGE">
+
+                    <input type="hidden" value="qqfUB8Zmp6+h4NfDpm6ZA03C0bqCmSpKU97J3jJLHNsdDdpCX2s5ncT5Zl5fbfnf0WALGEufxdeQVU6mLdZk+zhDq8696a4uwg6HE2OphdiPXlSvpadq57jN+GHuB3QG" data-form-email="true">
+
+                    
+
+                    <div class="form-group">
+                        <label class="form-control-label" for="contacts5-b-email">Email<span class="form-asterisk">*</span></label>
+                        <input type="email" class="form-control input-sm input-inverse" name="email" required="" data-form-field="Email" id="contacts5-b-email">
+                    </div>
+
+                    
+
+                    <div class="form-group">
+                        <label class="form-control-label" for="contacts5-b-message">Message</label>
+                        <textarea class="form-control input-sm input-inverse" name="message" data-form-field="Message" rows="5" id="contacts5-b-message"></textarea>
+                    </div>
+
+                    <div><button type="submit" class="btn btn-sm btn-info">Contact us</button></div>
+
+                </form>
+
+            </div>
+        </div>
+</section>
+
+<footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="footer1-2" style="background-color: rgb(50, 50, 50); padding-top: 1.75rem; padding-bottom: 1.75rem;">
+    
+    <div class="container">
+        <p class="text-xs-center">Copyright (c) 2017.</p>
+    </div>
+</footer>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smooth-scroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touch-swipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/viewport-checker/jquery.viewportchecker.js"></script>
+  <script src="assets/jarallax/jarallax.js"></script>
+  <script src="assets/jquery-mb-ytplayer/jquery.mb.ytplayer.min.js"></script>
+  <script src="assets/social-likes/social-likes.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/formoid/formoid.min.js"></script>
+  
+  
+  <input name="animation" type="hidden">
+   <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i class="mbr-arrow-up-icon"></i></a></div>
+  </body>
+</html>
